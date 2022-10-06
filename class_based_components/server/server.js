0210f8cc44729cb0cb5a8ef2b5caec7a72ceba14
Jake Robins <me@jakerobins.com>
2022-10-06 10:25:17 -0500

    small fixes to frogserve

--- class_based_components/server/server.js
+++ class_based_components/server/server.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const frogs = require("./frogs.json");
 
+const PORT = 8080;
+
 app.get("/frogs", (req, res) => {
   res.json(frogs);
 });
 
 app.get("/frogs/random", (req, res) => {
-  const randomNumber = Math.floor(Math.random() * 10);
+  const randomNumber = Math.floor(Math.random() * frogs.length);
   res.json(frogs[randomNumber]);
 });
 
-app.listen(8080, () =>
+app.listen(PORT, () =>
   console.log(
-    "🐸🐸🐸🐸🐸🐸 Prepare yourself for some frogs on Port 8080 🐸🐸🐸🐸🐸🐸"
+    `🐸🐸🐸🐸🐸🐸 Prepare yourself for some frogs on Port ${PORT} 🐸🐸🐸🐸🐸🐸`
   )
 );
