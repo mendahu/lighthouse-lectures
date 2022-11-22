16ed6e4dcfa0c3817c2aa432f930b27263b7e624
Jake Robins <me@jakerobins.com>
2022-11-22 13:54:19 -0600

    add lecture notes

--- objects_in_js/lecture.md
+++ objects_in_js/lecture.md
@@ -7,3 +7,81 @@
 - [ ] Passing primitives and objects to functions
 - [ ] Functions inside objects (using `this`)
 - [ ] Object iteration with `for..in`
+
+## Data
+
+### Primitives
+
+- There are 7 Primitive Data Types in JS
+  - string
+  - bigint
+  - boolean
+  - Symbol
+  - number
+  - null
+  - undefined
+- Primitives are immutable
+
+### Objects
+
+- Why use an object?
+  - Container with special characteristics
+  - Store more linked information than a primitive
+  - Better define/charcterize data
+  - Cooked meal vs ingredients
+  - Helps namespace data
+
+### Working with Objects
+
+- Reading Objects
+  - Square Bracket Notation for specific keys or dynamic keys
+  - dot notation for specific keys only!!!!1
+- Creating Objects
+  - Class COnstructor (`new Object()`)
+  - Omit the "new" (`Object()`)
+  - Object Literal (`= {}`)
+  - Empty Objects are allowed (`{}`)
+- Writing Values to OBjects / Changing values
+  - Use the same syntax as reading (square bracket or dot), but use the assignment operator (`=`)
+
+### Passing Data into Functions
+
+- Primitives are passed in to functions by their value
+  - the parameter name is a new variable with the same value
+  - the original variable remains untouched
+- Objects are passed by reference
+  - the parameter name is pointing to the same place as the original variable name
+  - two labels on the same drawer!!!!1
+
+### Functions as object values
+
+- Functions can be stored as values in objects
+- `this` can be used in a function to refer to parent object
+  - be careful with function expression vs declaration - it will change context of `this`
+  - be careful with nested functions, it will change the context of `this`!!!
+
+### Objects vs Arrays
+
+- Why would we use an Object vs an Array?
+- Objects
+  - the data is related but the pieces are their own special snowflalkes, have their own identity or meaning
+  - When you plan on working with a specific part of the data at one time
+- Arrays
+  - Iterate over data, array is _probably_ correct
+  - The order of the data is important
+  - Length is important, quantity of data
+
+### Iteration
+
+- Looping over data is a very "array"-like behaviour
+- BUT
+- Objects can be iterated over
+- Array
+  - `for (const el of array)`
+  - [1, 2, 3] -> el == 1 or 2 or 3
+  - `.forEach((el) => //do something)`
+- Objects
+  - `for (const key in object)`
+  - { name: "Jake", age: 37 }
+  - key = "name" or "age"
+  - object[key]
---
+++ objects_in_js/test.js
@@ -0,0 +1,129 @@
+// const myName = "Jake";
+// keyword const
+// declares a new variable, give it a name
+// assign a value to the variable
+// value ("Jake")
+
+// console.log(myName);
+
+// let message = "Hello";
+// // message[0] = "C"
+// message = "Cello";
+// console.log(message[0]);
+
+// const name = "Jake"
+// const age = 37
+// const isInstructor = true
+
+// const name = "David"
+// const age = 30
+// const isInstructor = false
+
+// const person1 = {
+//   name: "Jake",
+//   age: 37,
+//   isInstructor: true,
+// };
+
+// const person2 = {
+//   name: "David",
+//   age: 30,
+//   isInstructor: false,
+//   // 37: "Hello",
+// };
+
+// console.log(person1["name"]);
+// // console.log(person1.name);
+
+// const users = {
+//   "me@jakerobins.com": "Jake Robins",
+//   "david@david.com": "David Sooley",
+//   // ...
+// };
+
+// const email = "me@jakerobins.com";
+
+// console.log(users[email]);
+// console.log(users.email); // will not work
+
+// const myObj = {
+//   // Object Literal
+//   key: "value",
+// };
+
+// const myObj2 = new Object({
+//   key: "value",
+// });
+
+// const myObj3 = Object({
+//   key: "value",
+// });
+
+// const myObj4 = {};
+
+// console.log(myObj);
+// console.log(myObj2);
+// console.log(myObj3);
+
+// const myObj = {
+//   name: "Jake", // can be anything that you can store in a variable
+//   age: 37,
+//   isInstructor: true,
+//   address: {
+//     city: "Merida",
+//     country: "Mexico",
+//   },
+// };
+
+// myObj.name = "Jake";
+// myObj["name"] = "Jake";
+// myObj["address"]["city"];
+// myObj.address.city;
+// myObj.address["city"]; // why would you do this??????
+// myObj["address"].city;
+
+// console.log(myObj["address"]["city"]);
+
+// Primitives in functions
+// const age = 37;
+
+// const printNumPlusOne = (num) => {
+//   num = num + 1;
+//   console.log(num);
+// };
+
+// printNumPlusOne(age);
+// console.log(age);
+
+// const jake = {
+//   name: "Jake",
+//   age: 37,
+//   isInstructor: true,
+// };
+
+// const printAgeAfterOneYear = (person) => {
+//   person.age = person.age + 1;
+//   console.log(person.age);
+// };
+
+// printAgeAfterOneYear(jake);
+// console.log(jake.age);
+
+// const jake = {
+//   name: "Jake",
+//   age: 37,
+//   isInstructor: true,
+//   cohorts: ["Nov142022", "May162022"],
+//   abilities: {
+//     sayHello: function () {
+//       console.log("Hello!!");
+//     },
+//     makeYounger: function () {
+//       jake.age = jake.age - 1; // this changes!!!
+//     },
+//   },
+// };
+
+// console.log(jake.age);
+// jake.abilities.makeYounger();
+// console.log(jake.age);
