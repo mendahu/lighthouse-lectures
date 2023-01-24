16248d69a3c3f9c2860de5945a03647fef24fc16
Jake Robins <me@jakerobins.com>
2023-01-24 13:52:59 -0600

    add lecture notes

--- ajax/lecture.md
+++ ajax/lecture.md
@@ -6,3 +6,23 @@
 [ ] - What is AJAX?
 [ ] - History of AJAX
 [ ] - Demo - Fetching Mars Robot Times
+
+### What is Ajax??
+
+- AJAX stands for Asynchornous Javascript And XML
+- AJAX is what we use to send and recieve data asynchronously without requiring a browser refresh
+- AJAX is not a technology per se - it's more of a design principle
+
+* What is XML??
+* Is it like HTML???? ML - Markup Language
+* X is eXtensible...
+*
+
+### History on AJAX
+
+- Invented by Microsoft
+- AJAX is basically responsible for Web 2.0
+- Gmail Google Maps, big early pioneers of AJAX
+- Originally used with XML, but now more JSON these days
+
+RETURN AT 11:05 PST 2:05 EST
--- ajax/public/index.js
+++ ajax/public/index.js
@@ -1,3 +1,100 @@
 $(document).ready(() => {
-  // JS File
+  const createListItem = (robot) => {
+    return $(`
+    <article>
+      <span>${robot.name}</span>
+      <button class="button">Load</button>
+    </article>`);
+  };
+
+  const createMainDisplay = (robot) => {
+    return $(`
+      <img
+        width="400px"
+        src=${robot.bannerUrl}
+        alt=${robot.spacecraft.name}
+      />
+      <div class="inner-content">
+        <h3>${robot.spacecraft.name}</h3>
+        <p>Current time is: ${robot.currentTime} LMST</p>
+        <p>Current Sol is: ${robot.currentSol}</p>
+      </div>
+    `);
+  };
+
+  const generateApiErrorCard = () => {
+    return $(`
+      <div class="inner-content">
+        <h3>API Error</h3>
+        <p>You put in a bad code</p>
+        <p>Try again please</p>
+      </div>
+    `);
+  };
+
+  const fetchSpacecraft = (code) => {
+    $.get(`https://www.marsti.me/api/spacecraft/${code}`)
+      .then((robot) => {
+        const $display = createMainDisplay(robot);
+        $(".main-display").empty().append($display);
+      })
+      .catch((err) => {
+        const $display = generateApiErrorCard();
+        $(".main-display").empty().append($display);
+      });
+  };
+
+  const populateList = (spacecraft) => {
+    $(".spacecraft-list").empty();
+    for (const robot of spacecraft) {
+      const $listItem = createListItem(robot);
+
+      $listItem.find("button").click(() => {
+        fetchSpacecraft(robot.id);
+      });
+
+      $(".spacecraft-list").append($listItem);
+    }
+  };
+
+  const getSpacecraft = () => {
+    // XMLHttpRequest
+
+    // const req = new XMLHttpRequest();
+    // req.addEventListener("load", function () {
+    // $(".spacecraft-list").empty();
+    // for (const robot of JSON.parse(this.responseText)) {
+    //   $(".spacecraft-list").append(createListItem(robot));
+    // }
+    // });
+    // req.open("GET", "https://www.marsti.me/api/spacecraft");
+    // req.send();
+
+    // Fetch API
+
+    // fetch("https://www.marsti.me/api/spacecraft")
+    //   .then((res) => res.json())
+    //   .then((spacecraft) => populateList(spacecraft)) // could just pass reference in
+    //  .catch((err) => console.error(err));
+    // jQuery
+
+    // $.ajax({
+    //   method: "GET",
+    //   url: "https://www.marsti.me/api/spacecraft",
+    // })
+    //   .then((spacecraft) => populateList(spacecraft))
+    //   .catch((err) => console.error(err));
+
+    $.get("https://www.marsti.me/api/spacecraft")
+      .then((spacecraft) => populateList(spacecraft))
+      .catch((err) => console.error(err));
+  };
+
+  $(".fetcher form").submit(function (event) {
+    event.preventDefault();
+    const code = $(this).find("input").val();
+    fetchSpacecraft(code);
+  });
+
+  getSpacecraft();
 });
