064715c2659532569b252ce81a117fe51345d5ea
Jake Robins <me@jakerobins.com>
2024-01-23 14:36:46 -0600

    add lecture notes

--- ajax/lecture.md
+++ ajax/lecture.md
@@ -6,3 +6,28 @@
 [ ] - What is AJAX?
 [ ] - History of AJAX
 [ ] - Demo - Fetching Mars Robot Times
+
+# Client rendered vs Server Rendered
+
+- See diagrams
+
+# AJAX
+
+- Asynchronous Javascript And XML
+  - XML - eXtensible Markup Language
+- Thanks to AJAX we can send and recieve data asynchronously without reqiuring a browser refresh
+
+  1. Type in URL to browser and hit enter - GET Request
+  2. Forms - POST or GET
+  3. AJAX is the third way
+
+  - Not a technology, its a principle of programming
+
+- Invented by Microsoft for Outlook Web Access
+- AJAX proliferated Web 2.0
+- Google was big proponent - Maps and Gmail both did a lot of AJAX
+- The original data structure for AJAX resposnes was XML - but today we mostly use JSON
+
+- sending HTTP requests with JavaScript
+
+:12 past the hour
--- ajax/public/index.js
+++ ajax/public/index.js
@@ -1,3 +1,106 @@
 $(document).ready(() => {
-  // JS File
+  const createMainDisplay = (spacecraft) => {
+    return $(`
+      <img width="400px" src="${spacecraft.bannerUrl}"
+       alt="${spacecraft.spacecraft.name} on Mars!" />
+       <div class="inner-content">
+        <h3>${spacecraft.spacecraft.name}</h3>
+        <p>Current time is: ${spacecraft.currentTime} LMST</p>
+        <p>Current sol is: ${spacecraft.currentSol}</p>
+       </div>
+    `);
+  };
+
+  const renderMainDisplay = (spacecraft) => {
+    const $display = createMainDisplay(spacecraft);
+    $(".main-display").empty().append($display);
+  };
+
+  const getIndividualSpacecraft = (id) => {
+    $.get(`https://www.marsti.me/api/spacecraft/${id}`).then((spacecraft) =>
+      renderMainDisplay(spacecraft)
+    );
+  };
+
+  const createListItem = (spacecraft) => {
+    return $(`
+        <article>
+          <span>${spacecraft.name}</span>
+          <button class="button">Load</button>
+        </article>
+      `);
+  };
+
+  const renderList = (data) => {
+    const $spacecraftList = $(".spacecraft-list");
+    $spacecraftList.empty();
+
+    for (const spacecraft of data) {
+      const $spacecraft = createListItem(spacecraft);
+
+      $spacecraft.find("button").click(() => {
+        getIndividualSpacecraft(spacecraft.id);
+      });
+
+      $spacecraftList.append($spacecraft);
+    }
+  };
+
+  const getSpacecraft = () => {
+    console.log("getSpacecraft called");
+
+    // XHR Way to do it
+
+    // const xhr = new XMLHttpRequest();
+    // xhr.addEventListener("load", function () {
+    //   renderList(JSON.parse(this.responseText));
+    // });
+    // xhr.open("GET", "https://www.marsti.me/api/spacecraft");
+    // xhr.send();
+
+    // Fetch API
+
+    // fetch("https://www.marsti.me/api/spacecraft")
+    //   .then((response) => response.json())
+    //   .then((data) => renderList(data));
+
+    // jQuery Way to do it
+
+    // $.ajax({
+    //   method: "GET",
+    //   url: "https://www.marsti.me/api/spacecraft",
+    // }).then((res) => renderList(res));
+
+    // jQuery convenience method function
+
+    $.get("https://www.marsti.me/api/spacecraft").then((res) =>
+      renderList(res)
+    );
+  };
+
+  // Load the list on click
+
+  // const $loadButton = $(".spacecraft-list").find("button");
+  // $loadButton.click(() => {
+  // });
+
+  // target the form
+  $(".fetcher form").on("submit", function (event) {
+    // use function declartion for jQuery event handlers!!!
+
+    // override the submit event
+    event.preventDefault();
+
+    // serialize the data for manual transmission
+    console.log($(this).serializeArray());
+
+    // write our own logic
+    const $input = $(".fetcher form input");
+    const id = $input.val();
+
+    getIndividualSpacecraft(id);
+  });
+
+  getSpacecraft();
+  // XHR
 });
